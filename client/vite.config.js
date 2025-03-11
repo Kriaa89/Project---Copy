112fd5a0cd815575c89566d0a8db1662c11fee6e
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-03-12 00:59:32 +0100

    Remove unused Tailwind CSS configuration, clean up index.html by removing unnecessary script, and update Vite configuration for improved server settings.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   server: {
-    host: true,
-    port: 3000,
-    strictPort: true,
-    open: true
+    port: 5173
   }
 })
